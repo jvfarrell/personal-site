22fcfabe2ea39f1e0bd5addbe6b6c13ba802a622
jvfarrell <jvictorfarrell@gmail.com>
2017-06-19 15:50:49 -0400

    adding placeholder to input field, fixing layout for mobile

--- lol/forms.py
+++ lol/forms.py
@@ -2,4 +2,4 @@ from django import forms
 
 
 class SummonerForm(forms.Form):
-    summoner_name = forms.CharField(label='Summoner Name')
+    summoner_name = forms.CharField(label='Summoner Name', widget=forms.TextInput(attrs={'placeholder': 'Summoner Name'}))
